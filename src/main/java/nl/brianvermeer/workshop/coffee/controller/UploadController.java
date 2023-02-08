d235f1f5f87d7740731b185241a2902d09c68faa
Brian Vermeer <brianvermeer@snyk.io>
2023-02-08 16:03:04 +0100

    clean up

--- src/main/java/nl/brianvermeer/workshop/coffee/controller/UploadController.java
+++ src/main/java/nl/brianvermeer/workshop/coffee/controller/UploadController.java
@@ -4,7 +4,6 @@ import nl.brianvermeer.workshop.coffee.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +11,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.Principal;
 
@@ -29,12 +27,10 @@ public class UploadController {
     }
 
     @PostMapping("/uploadimage") public String uploadImage(Model model, @RequestParam("image") MultipartFile file, Principal principal) throws IOException {
-        StringBuilder fileNames = new StringBuilder();
         var name = file.getOriginalFilename().replace(" ", "_");
-        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, name);
-        fileNames.append(name);
+        var fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, name);
         Files.write(fileNameAndPath, file.getBytes());
-        model.addAttribute("msg", "Uploaded images: " + fileNames.toString());
+        model.addAttribute("msg", "Uploaded images: " + name);
 
         if (principal == null) {
             model.addAttribute("message", "ERROR");
@@ -46,8 +42,6 @@ public class UploadController {
 
         person.setProfilePic(name);
         personService.savePerson(person);
-
-
         return "person/upload";
     }
 }
