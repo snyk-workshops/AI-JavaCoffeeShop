c2bbcf7504e77e2418db94f6e37dbf416a948256
Micah Silverman <micah@afitnerd.com>
2024-03-06 10:52:43 -0500

    super awesome search, yo

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -19,7 +19,15 @@ public class SearchRepository {
     DataSource dataSource;
 
     public List<Product> searchProduct (String input) {
-        return null;
+        // lowercase the input
+        var lowerInput = input.toLowerCase(Locale.ROOT);
+        // create a query matching the input to the product or the descriptiom
+        var query = em.createNativeQuery("Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'", Product.class);
+        // get the result list
+        var resultList = (List<Product>) query.getResultList();
+        return resultList;
+
+
     }
 
 }
